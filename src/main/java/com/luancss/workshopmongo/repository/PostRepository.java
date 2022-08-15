332f308ffbd55bbe6b5adbe969d62665b1f04fde
Luancss <ventobo123@gmail.com>
2022-08-15 20:39:34 -0300

    Criando entity Post com user aninhado

---
+++ src/main/java/com/luancss/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,14 @@
+package com.luancss.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luancss.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+	
+	
+	
+}
