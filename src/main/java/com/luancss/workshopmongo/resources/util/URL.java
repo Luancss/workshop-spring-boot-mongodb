2ff6eb941fd515bcbe4d5004f2f3513c02477b14
Luancss <ventobo123@gmail.com>
2022-08-17 20:23:16 -0300

    Consulta com varios criterios

--- src/main/java/com/luancss/workshopmongo/resources/util/URL.java
+++ src/main/java/com/luancss/workshopmongo/resources/util/URL.java
@@ -2,6 +2,10 @@ package com.luancss.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 
@@ -12,4 +16,15 @@ public class URL {
 			return "";
 		}
 	}
+
+	public static Date convertDate(String textDate, Date defaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(textDate);
+		} catch (ParseException e) {
+			return defaultValue;
+		}
+	}
+
 }
